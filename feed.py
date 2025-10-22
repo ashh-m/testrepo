b9904d135aedc09b9debf8fc7ce5da60302ecff1
Ashley Miltenberger <ashley.miltenberger@csuglobal.edu>
2025-10-22 04:51:56 +0000

    Refactor feed.py for improved readability and maintainability

--- feed.py
+++ feed.py
@@ -4,15 +4,14 @@ import xml.etree.ElementTree as xml_tree
 with open('feed.yaml', 'r') as file:
     yaml_data = yaml.safe_load(file)
 
-    rss_element = xml_tree.Element('rss', {
-        'version': '2.0',
+    rss_element = xml_tree.Element('rss', {'version': '2.0',
         'xmlns:itunes': 'http://www.itunes.com/dtds/podcast-1.0.dtd',
-        'xmlns:content': 'http://purl.org/rss/1.0/modules/content/'
-    })
-
-link_prefix = yaml_data['link']
+        'xmlns:content': 'http://purl.org/rss/1.0/modules/content/'})
 
 channel_element = xml_tree.SubElement(rss_element, 'channel')
+
+link_prefix = yaml_data.get('link', '')
+
 xml_tree.SubElement(channel_element, 'title').text = yaml_data['title']
 xml_tree.SubElement(channel_element, 'format').text = yaml_data['format']
 xml_tree.SubElement(channel_element, 'itunes:subtitle').text = yaml_data['subtitle']
